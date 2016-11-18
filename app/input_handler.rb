a00712975724f863e58f95c90fdf5020a15aad21
Rob Hesketh <rhesketh.uk@gmail.com>
2016-11-18 20:56:02 +0000

    InputHandler can handle New Image command (I) and check that the arguments given are valid

--- app/input_handler.rb
+++ app/input_handler.rb
@@ -1,5 +1,29 @@
 class InputHandler
   def parse(input)
-    puts "unrecognised command :("
+    command, *arguments = input.split(" ")
+
+    case command
+    when "I"
+      if new_image_arguments_are_invalid?(arguments)
+        puts "ERROR: invalid argument(s)"
+        puts "  Valid format is `I [x] [y]`"
+        puts "  E.g. `I 5 5`"
+      end
+    else
+      puts "unrecognised command :("
+    end
+  end
+
+  private
+
+  def new_image_arguments_are_invalid?(arguments)
+    return true if arguments.length != 2
+    return true if !is_a_number?(arguments[0]) || !is_a_number?(arguments[1])
+
+    return false
+  end
+
+  def is_a_number?(argument)
+    return /\A\d+\z/.match(argument)
   end
 end
--- spec/bitmap_editor/input_handler_spec.rb
+++ spec/bitmap_editor/input_handler_spec.rb
@@ -8,4 +8,33 @@ describe 'InputHandler' do
       expect { handler.parse("F") }.to output(/unrecognised command :\(/).to_stdout
     end
   end
+
+  context "When the 'new image' command is given" do
+    context "With valid image dimensions" do
+      it "does not output anything" do
+         expect { handler.parse("I 5 5") }.to_not output.to_stdout
+      end
+    end
+
+    context "With not enough arguments" do
+      it "outputs an error" do
+        expect { handler.parse("I") }.to output(/invalid argument\(s\)/).to_stdout
+        expect { handler.parse("I 5") }.to output(/invalid argument\(s\)/).to_stdout
+      end
+    end
+
+    context "With too many arguments" do
+      it "outputs an error" do
+        expect { handler.parse("I 5 5 5") }.to output(/invalid argument\(s\)/).to_stdout
+      end
+    end
+
+    context "With arguments that are not integers" do
+      it "outputs an error" do
+        expect { handler.parse("I 5 Y") }.to output(/invalid argument\(s\)/).to_stdout
+        expect { handler.parse("I X 5") }.to output(/invalid argument\(s\)/).to_stdout
+        expect { handler.parse("I X Y") }.to output(/invalid argument\(s\)/).to_stdout
+      end
+    end
+  end
 end
