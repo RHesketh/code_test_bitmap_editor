1fd276d798d6877b8a31bfa4d3f63a48a62a915e
Rob Hesketh <rhesketh.uk@gmail.com>
2016-11-20 00:35:23 +0000

    Only redirect stderr during rspec tests so that we can still debug.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,14 +10,11 @@ RSpec.configure do |config|
 
   # Redirect stdout and stderr during rspec runs so their output doesn't show up in the results. 
   original_stderr = $stderr
-  original_stdout = $stdout
   config.before(:all) do
     # Redirect stderr and stdout
     $stderr = File.open(File::NULL, "w")
-    $stdout = File.open(File::NULL, "w")
   end
   config.after(:all) do
     $stderr = original_stderr
-    $stdout = original_stdout
   end
 end
